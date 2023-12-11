132b32ec91160290d3ba02ce192d803590c77fde
xd_multiplayer <variusluden@gmail.com>
2023-12-11 11:12:58 -0500

    217. Contains Duplicate

---
+++ Arrays & Hashing/Contains Duplicate.ts
@@ -0,0 +1,7 @@
+function containsDuplicate(nums: number[]): boolean {
+    return new Set(nums).size < nums.length;
+}
+
+console.log(containsDuplicate([1,2,3,1]));
+console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
