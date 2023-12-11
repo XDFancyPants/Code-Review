2de758b7de9b158d8d5695aca42922f9af6c8479
xd_multiplayer <variusluden@gmail.com>
2023-12-11 11:22:37 -0500

    217. Contains Duplicate, less memory solution

--- Arrays & Hashing/Contains Duplicate.ts
+++ Arrays & Hashing/Contains Duplicate.ts
@@ -2,6 +2,25 @@ function containsDuplicate(nums: number[]): boolean {
     return new Set(nums).size < nums.length;
 }
 
+/**
+ * Less memory solution, without using Set, way slower
+ */
+
+function containsDuplicate2(nums: number[]): boolean {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 console.log(containsDuplicate([1,2,3,1]));
 console.log(containsDuplicate([1,2,3,4]));
 console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
+
+console.log(containsDuplicate2([1,2,3,1]));
+console.log(containsDuplicate2([1,2,3,4]));
+console.log(containsDuplicate2([1,1,1,3,3,4,3,2,4,2]));
