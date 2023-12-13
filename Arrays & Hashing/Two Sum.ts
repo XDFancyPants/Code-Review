1869c3a0810d3323960393931ea7e42c4c43721d
xd_multiplayer <variusluden@gmail.com>
2023-12-13 11:36:02 -0500

    1. Two Sum

---
+++ Arrays & Hashing/Two Sum.ts
@@ -0,0 +1,69 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        const num2 = nums.indexOf(target-nums[i]);
+        if (num2 > -1 && i !== num2) {
+            return [i, num2];
+        }
+    }
+    return [-1, -1];
+};
+
+// In Typescript, using a Map is faster for some reason:
+function twoSum2(nums: number[], target: number): number[] {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const diff = target-num;
+
+        if(map.has(diff)) {
+            return [map.get(diff), i]
+        }
+
+        map.set(num, i);
+    }
+
+    return [-1, -1];
+}
+
+
+console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([3,2,4], 6));
+
+console.log(twoSum2([2,7,11,15], 9));
+console.log(twoSum2([3,2,4], 6));
+
+/**
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+ */
